451067a181f79f00fe54c0a9f35c2432ba521870
Lynn <you@example.com>
2020-05-04 13:29:48 +0000

    Add Unique ID

--- Sightings/models.py
+++ Sightings/models.py
@@ -7,8 +7,14 @@ class squirrel(models.Model):
         help_text=_('Latitude'),
         max_length=100,
         )
+
     Longitude = models.FloatField(
         help_text=_('Longitude'),
+        max_length=100,
+        )
+
+     UniqueID = models.CharField(
+        help_text_('Longitude'),
         max_length=100,
         )
 
