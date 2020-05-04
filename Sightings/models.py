615977cddb59ed3740cc00907037d0b72b99cca2
Lynn <you@example.com>
2020-05-04 13:20:44 +0000

    Add Longitude and Latitude

--- Sightings/models.py
+++ Sightings/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from django.utils.translation import gettext as _
+
+class squirrel(models.Model):
+    Latitude = models.FloatField(
+        help_text=_('Latitude'),
+        max_length=100,
+        )
+    Longitude = models.FloatField(
+        help_text=_('Longitude'),
+        max_length=100,
+        )
+
 
