92c4b928dd195bb8de8f7297486a316ebbfca3a7
Lynn <you@example.com>
2020-05-08 01:44:35 +0000

    Update urls.py

--- Sightings/urls.py
+++ Sightings/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.sighting),
-        path('<str:UniqueID/>',views.update_sighting),
-        path('add/',views.add),
-        path('stats/',views.stats),
+        path('', views.sighting, name = 'all squirrels'),
+        path('<str:UniqueID>/', views.update_sighting, name = 'update'),
+        path('add/', views.add , name = 'add'),
+        path('stats/', views.stats, name = 'stats'),
         ]
 
