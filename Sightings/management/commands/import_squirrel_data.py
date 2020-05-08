124c11d364234c42a563314169445ff8abb2d397
Kanonakias <kanonakias@gmail.com>
2020-05-08 17:04:05 +0000

    Changeimport file with our Var Names

--- Sightings/management/commands/import_squirrel_data.py
+++ Sightings/management/commands/import_squirrel_data.py
@@ -1,3 +1,6 @@
+from django.utils import timezone
+from Sightings.models import squirrel
+
 from django.core.management.base import BaseCommand
 import csv
 
@@ -9,12 +12,38 @@ class Command(BaseCommand):
         with open(options['csv_file'], encoding = 'utf-8') as fp:
             reader = csv.DictReader(fp)
             data = list(reader) 
+        
+        sightings = []
 
-        for x in data:
-            s = Squirrel(
-                Y = x['Y'],
-                X = x['X'],
-                UniqueID = x['Unique Squirrel ID'],
+        for dict_ in data:
 
-            )
-            s.save()
+            sightings.append(squirrel(
+                Y  = float(dict_['Y']),
+                X  = float(dict_['X']),
+                UniqueID = dict_['Unique Squirrel ID'],
+                Shift = dict_['Shift'].lower,
+                Date = timezone.datetime.strptime(dict_['Date'],'%m%d%Y').date(),
+                Age = dict_['Age'],
+                Fur = dict_['Primary Fur Color'],
+                Location = dict_['Location'],
+                Spec_location = dict_['Special Location'],
+                Running = dict_['Running'.loweir()=='true', 
+                Chasing = dict_['Chasing'].lower()=='true',
+                Climbing = dict_['Climbing'].lower() == 'true',
+                Eating = dict_['Eating'].lower == 'true',
+                Foraging = dict_['Foraging'].lower() == 'true',
+                Other_activities = dict_['Other Activities'],
+                Kuks = dict_['Kuks'].lower() == 'true',
+                Quaas = dict_['Quaas'].lower() ==  'true',
+                Moans = dict_['Moans'].lower() == 'true',
+                Tail_Flags = dict_['Tail flags'].lower() == 'true',
+                Tail_Twitches = dict_['Tail twitches'].lower() == 'true',
+                Approaches = dict_['Approaches'].lower() == 'true',
+                Indifferent = dict_['Indifferent'].lower() == 'true',
+                runs_from = dict_['Runs from'].lower() == 'true',
+                ))
+
+        squirrel.objects.bulk_create(sightings)
+
+
+
