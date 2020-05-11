58d9ee8e95437cdc6a36bb61ae6cb6698913abc7
Kanonakias <kanonakias@gmail.com>
2020-05-11 23:48:44 +0000

    Minor Mistake in urls

--- Sightings/urls.py
+++ Sightings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
         path('', views.sighting, name = 'all squirrels'),
-        path('<str:UniqueID>/', views.update_sighting, name = 'update'),
+        path('<UniqueID>/', views.update_sighting, name = 'update'),
         path('add/', views.add , name = 'add'),
         path('stats/', views.stats, name = 'stats'),
         path('map/', views.map, name = 'map'),
